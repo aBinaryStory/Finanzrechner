1e5b14102deb07ab2f098fe93de8518e07e9d0ef
aBinaryStory <abinarystory@gmail.com>
2020-02-16 12:47:24 +0100

    Projektdateien hinzufügen.

---
+++ Finanzrechner/FinanzrechnerBinding.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Finanzrechner
+{
+    /// <summary>
+    /// Interaktionslogik für FinanzrechnerBinding.xaml
+    /// </summary>
+    public partial class FinanzrechnerBinding : Window, INotifyPropertyChanged
+    {
+        public int MaxKredit => 50000;
+        public int MinKredit => 10000;
+        public int MaxJahre => 120;
+        public int MinJahre => 10;
+        public int MaxZinssatz => 10;
+        public int MinZinssatz => 1;
+
+        private int _kredit;
+
+        public int Kredit
+        {
+            get { return _kredit; }
+            set 
+            { 
+                if(SetProperty(ref _kredit, value))
+                {
+                    RaisePropertyChanged(nameof(Rate)); //oder SliderChangedEvent -> Notify(Rate)
+                }
+            }
+        }
+
+        private int _laufzeit;
+
+        public int Laufzeit
+        {
+            get { return _laufzeit; }
+            set 
+            { 
+                if (SetProperty(ref _laufzeit, value))
+                {
+                    RaisePropertyChanged(nameof(Rate));
+                }
+            }
+        }
+
+        private double _zinssatz;
+
+        public double Zinssatz
+        {
+            get { return _zinssatz; }
+            set
+            {
+                if (SetProperty(ref _zinssatz, value))
+                {
+                    RaisePropertyChanged(nameof(Rate));
+                }
+            }
+        }
+
+        public double Rate
+        {
+            get 
+            { 
+                double zwischenSumme = Math.Pow(1 + (Zinssatz / 1200), Laufzeit);
+                //Teilen durch Null verhindern
+                return zwischenSumme == 1 ? 0 : Kredit * (((Zinssatz / 1200) * zwischenSumme) / (zwischenSumme - 1));
+            }
+        }
+
+        public FinanzrechnerBinding()
+        {
+            Kredit = MinKredit;
+            Laufzeit = MinJahre;
+            Zinssatz = MinZinssatz;
+            InitializeComponent();
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value)) return false;
+
+            storage = value;
+            RaisePropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            PropertyChanged?.Invoke(this, args);
+        }
+        #endregion
+    }
+}
---
+++ Finanzrechner/FinanzrechnerNormal.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finanzrechner
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class FinanzRechnerNormal : Window
+    {
+        public FinanzRechnerNormal()
+        {
+            InitializeComponent();
+            lblKredit.Content = sldKredit.Value.ToString();
+            lblZinssatz.Content = sldZins.Value.ToString();
+            lblLaufzeit.Content = sldLaufzeit.Value.ToString();
+            RateBerechnen();
+        } 
+
+        private void Kreditsumme_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(IsInitialized)
+            {
+                lblKredit.Content = ((Slider)sender).Value.ToString();
+                RateBerechnen();
+            }
+        }
+
+        private void Zinzsatz_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(IsInitialized)
+            {
+                lblZinssatz.Content = ((Slider)sender).Value;
+                RateBerechnen();
+            }
+        }
+
+        private void Laufzeit_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(IsInitialized)
+            {
+                lblLaufzeit.Content = ((Slider)sender).Value;
+                RateBerechnen();
+            }
+        }
+
+        private void RateBerechnen()
+        {
+            double zwischenSumme = Math.Pow(1 + (sldZins.Value / 1200), sldLaufzeit.Value);
+            //Teilen durch Null verhindern
+            var kredit = zwischenSumme == 1 ? 0 : sldKredit.Value * (((sldZins.Value / 1200) * zwischenSumme) / (zwischenSumme - 1));
+
+            rRate.Text = String.Format("{0:0.00}", kredit);          
+        }
+    }
+}
